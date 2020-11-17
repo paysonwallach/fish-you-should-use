95a00954ba7683e7a6cabdb27e9bdd8374b9b5e7
Payson Wallach <payson@paysonwallach.com>
2020-11-17 21:28:30 +0100

    fix: search entire command string for aliases

--- functions/__ysu__check_git_aliases.fish
+++ functions/__ysu__check_git_aliases.fish
@@ -1,17 +1,18 @@
 function __ysu__check_git_aliases \
         --on-event fish_preexec
-    set --local tokens (string split --max 2 " " "$argv")
+    string match --quiet "sudo *" "$argv"; and return
 
-    string match --quiet --regex "^sudo" "$argv"; and return
-
-    if string match --quiet --regex "^git" "$argv"
+    if string match --quiet --regex "git *" "$argv"
         set --local found false
         git config --get-regexp "^alias\..+\$" | sort | while read key value
             set key (string split --max 2 "." "$key")[2]
+            set --local escaped_key (string escape --no-quote --style=regex "$key")
 
-            string match --quiet "$key" "$YSU__IGNORED_GIT_ALIASES"; and continue
+            string match --quiet --regex "$escaped_key" "$YSU__IGNORED_GIT_ALIASES"; and continue
 
-            if string match --quiet "git $value" "$argv"
+            set --local escaped_value (string escape --no-quote --style=regex "$value")
+
+            if string match --quiet --regex "git $escaped_value" "$argv"
                 __ysu__message "git alias" "$value" "git $key"
                 set found true
             end
