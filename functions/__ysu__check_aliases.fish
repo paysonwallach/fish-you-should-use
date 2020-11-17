68f989e9c8d6ca994d8db92a02ec1645abdd2cd9
Payson Wallach <payson@paysonwallach.com>
2020-11-17 21:28:30 +0100

    fix: search entire command string for aliases

--- functions/__ysu__check_aliases.fish
+++ functions/__ysu__check_aliases.fish
@@ -1,21 +1,20 @@
 function __ysu__check_aliases \
         --on-event fish_preexec
+    string match --quiet "sudo *" "$argv"; and return
+
     set --local found false
-    set --local tokens
-    set --local key
-    set --local value
-
-    string match --quiet --regex "^sudo" "$argv"; and return
-
     alias | sort | while read entry
         set entry (string replace "=" "" "$entry")
-        set tokens (string split --max 2 " " "$entry")
-        set key "$tokens[2]"
-        set value (string replace --regex '(?:[\"|\']([^,]*)[\"|\'])' '$1' "$tokens[3]")
+        set --local tokens (string split --max 2 " " "$entry")
+        set --local key "$tokens[2]"
+        set --local escaped_key (string escape --no-quote --style=regex "$key")
 
-        string match --quiet "$key" "$YSU__IGNORED_GLOBAL_ALIASES"; and continue
+        string match --quiet --regex "$escaped_key" "$YSU__IGNORED_GLOBAL_ALIASES"; and continue
 
-        if string match --quiet "$value" "$argv"
+        set --local value (string replace --regex '(?:[\"|\']([^,]*)[\"|\'])' '$1' "$tokens[3]")
+        set --local escaped_value (string escape --no-quote --style=regex "$value")
+
+        if string match --quiet --regex "$escaped_value" "$argv"
             __ysu__message "alias" "$value" "$key"
             set found true
         end
